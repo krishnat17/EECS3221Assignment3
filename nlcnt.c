76872601cb5924c92287e18fb09815d8411064a8
krishnat17 <krishnathevathasan@gmail.com>
2016-11-27 01:24:14 -0500

    passing arguments now - should be done for real this time

--- nlcnt.c
+++ nlcnt.c
@@ -8,67 +8,73 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
-typedef void(*test)(void);
+typedef void* (*loadedFunction)();
 
-int main (int argc, char **argv) {
+int main (int argc, char *argv[]) {
 
   const char *error;
-  void *module;
-  test demo;
+  char fileName[100];
+  void *handle;
+  loadedFunction func;
 
+  //argv[1] should be either the signifier to for reading or mapping
+  //argv[2] should be the file name
   if(argc != 3) {
     fprintf(stderr,"%s - Exactly 2 arguments only\n", argv[0]);
     exit(1);
   }
 
+  //Get file name
+  strcpy(fileName, argv[2]);
+
+  //Check if user wants to use reading method
   if (strncmp("reading", argv[1], strnlen(argv[1])) == 0){
-    printf("In reading\n");
 
     //Load library
-    module = dlopen("./reading.so", RTLD_LAZY);
-    if (!module) {
+    handle = dlopen("./reading.so", RTLD_LAZY);
+    if (!handle) {
       fprintf(stderr, "%s - Couldn't open reading.so\n", dlerror());
       exit(1);
     }
 
     //Get symbol
     dlerror();
-    demo = dlsym(module, "my_cnt");
+    *(void**)(&func) = dlsym(handle, "my_cnt");
     if (error = (dlerror())) {
       fprintf(stderr, "%s - Couldn't find my_cnt\n", error);
       exit(1);
     }
 
     //Call function in library
-    (*demo)();
+    (void)func(argv[2]);
 
-    //Close module
-    dlclose(module);
+    //Close handle
+    dlclose(handle);
 
   }
+  //Check if user wants to use mapping method
   else if (strncmp("mapping", argv[1], strnlen(argv[1])) == 0){
-    printf("In mapping\n");
 
     //Load library
-    module = dlopen("./mapping.so", RTLD_LAZY);
-    if (!module) {
+    handle = dlopen("./mapping.so", RTLD_LAZY);
+    if (!handle) {
       fprintf(stderr, "%s - Couldn't open mapping.so\n", dlerror());
       exit(1);
     }
 
     //Get symbol
     dlerror();
-    demo = dlsym(module, "my_cnt");
+    *(void**)(&func) = dlsym(handle, "my_cnt");
     if (error = (dlerror())) {
       fprintf(stderr, "%s - Couldn't find my_cnt\n", error);
       exit(1);
     }
 
     //Call function in library
-    (*demo)();
+    (void)func(argv[2]);
 
-    //Close module
-    dlclose(module);
+    //Close handle
+    dlclose(handle);
 
   }
   else {
