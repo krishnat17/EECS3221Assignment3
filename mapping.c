172ce20a89a80974b9bc2dbdb71cc4294b66b117
krishnat17 <krishnathevathasan@gmail.com>
2016-11-27 01:22:30 -0500

    finished mapping.c

--- mapping.c
+++ mapping.c
@@ -6,7 +6,48 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-void my_cnt(void) {
-  printf("test in mapping\n");
+void my_cnt(char* fileName) {
+
+  int fd;
+  int i = 0;
+  int count = 0;
+  struct stat mystat;
+  char *map;
+
+  fd = open(fileName, O_RDONLY);
+  if (fd == -1)  {
+    fprintf(stderr, "mapping.so - Couldn't open %s \n", fileName);
+    exit(1);
+  }
+
+  if(fstat(fd,&mystat) < 0) {
+    fprintf(stderr, "mapping.so - fstat error\n");
+    close(fd);
+    exit(1);
+  }
+
+  map = mmap((caddr_t)0, mystat.st_size, PROT_READ, MAP_SHARED, fd, 0);
+  if (map == MAP_FAILED) {
+    fprintf(stderr, "mapping.so - mmap error\n");
+    close(fd);
+    exit(1);
+  }
+
+  while (i < mystat.st_size) {
+    if (map[i] == '\n' ) {
+      count++;
+    }
+    i++;
+  }
+
+  printf("There are %d newline character(s) in file %s \n", count, fileName);
+  close(fd);
+
+
 }
